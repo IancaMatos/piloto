5b411808e4782d052ca305623f201e3a8bbf37bf
Ianca Matos <iancaddd@gmail.com>
2025-11-27 14:38:44 -0300

    configurando home

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("A view index funcionou, wow")
+
+def sobre(request):
+    return HttpResponse( <h1>"Sistema 1.0 desenvolvido por mim"</h1>)
