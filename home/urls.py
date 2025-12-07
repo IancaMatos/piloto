2d27e240e50dd248e78f753faae464d9ba8b22ba
Ianca Matos <iancaddd@gmail.com>
2025-12-07 19:36:26 -0300

    exercicio 2

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('sobre/', views.sobre,name='sobre'),
     path('contato/', views.contato,name='contato'),
     path('ajuda/', views.ajuda,name='ajuda'),
+
+    path('item/<int:id>/', views.exibir_item,name='exibir_item'),
+    path('perfil/<str:usuario>/', views.perfil,name='perfil'),
+    path('diasemana/<int:dia>/', views.dia_da_semana, name='dia_da_semana'),
    
 ]
 
--- home/views.py
+++ home/views.py
@@ -15,3 +15,27 @@ def contato(request):
 
 def ajuda(request):
     return render(request, 'Ajuda.html')
+
+def exibir_item(request, id):
+    return render(request, 'exibir_item.html',{'id':id})
+
+def perfil(request, usuario):
+    return render(request, 'perfil.html', {'usuario': usuario})
+
+def dia_da_semana(request, dia):
+    dias_semana = {
+        "1":"Domingo",
+        "2":"Segunda-Feira",
+        "3":"Terça-Feira",
+        "4":"Quarta-Feira",
+        "5":"Quinta-Feira",
+        "6":"Sexta-Feira",
+        "7":"Sábado"
+    }
+    
+    dia_nome = dias_semana.get(str(dia))
+    
+    if dia_nome:
+        return render(request, 'dia_da_semana.html', {'dia': dia_nome})
+    else:
+        return render(request, 'dia_da_semana.html', {'dia': "Dia inválido"})
